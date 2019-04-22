14309a7a9ea1d8eb6f780b27b04b1a3f16e7d3f7
Sairama Krishna Bonala <sairama.bonala@wavemaker.com>
2019-04-22 06:38:11 +0000

    Initial Push from master

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "bricks",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobile_Localization_9",
+  "homePage" : "Main",
+  "name" : "Mobile_Localization_9",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en,hi-in,te-in",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
